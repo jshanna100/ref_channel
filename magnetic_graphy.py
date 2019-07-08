5b8a07a8e9ebb723be179bf478484ea9d51378e9
jeff <jeff.hanna@gmail.com>
2019-07-08 23:12:20 +0200

    b

--- magnetic_graphy.py
+++ magnetic_graphy.py
@@ -1,35 +1,30 @@
 import numpy as np
+import mne
+from mne.viz.backends.renderer import _Renderer
 from mayavi import mlab
 
-def dipole(m, r, r0):
-    """Calculate a field in point r created by a dipole moment m located in r0.
-    Spatial components are the outermost axis of r and returned B.
-    """
-    # we use np.subtract to allow r and r0 to be a python lists, not only np.array
-    R = np.subtract(np.transpose(r), r0).T
+subjects_dir = "/home/jeff/freesurfer/subjects"
+subject = "reftest"
+surface = "white"
+rawfile = "/home/jeff/ATT_dat/proc/nc_ATT_10_1_hand_ica-raw.fif"
+raw = mne.io.Raw(rawfile)
 
-    # assume that the spatial components of r are the outermost axis
-    norm_R = np.sqrt(np.einsum("i...,i...", R, R))
+#renderer = _Renderer(bgcolor=(0, 0, 0), size=(800, 800))
+a = mne.viz.plot_alignment(subject=subject,subjects_dir=subjects_dir,
+                           surfaces="brain",meg=["sensors","ref"],
+                           info=raw.info)
+mlab.quiver3d(0,0.25,-0.25,0,-0.25,0.45,color=(1,0,0))
+mlab.quiver3d(0,0,0,0,0,1, color=(1,1,0), scale_factor=0.18)
 
-    # calculate the dot product only for the outermost axis,
-    # that is the spatial components
-    m_dot_R = np.tensordot(m, R, axes=1)
 
-    # tensordot with axes=0 does a general outer product - we want no sum
-    B = 3 * m_dot_R * R / norm_R**5 - np.tensordot(m, 1 / norm_R**3, axes=0)
-
-    # include the physical constant
-    B *= 1e-7
-
-    return B
-
-X = np.linspace(-10, 10, 200)
-Y = np.linspace(-10, 10, 200)
-Z = np.linspace(-10, 10, 200)
-r = np.meshgrid(X, Y, Z)
-r0 = [0,0,0]
-m = [0.0, 0.0, 0.01]
-Bx, By, Bz = dipole(m=m, r=r, r0=r0)
-mlab.quiver3d(r[0],r[1],r[2],Bx,By,Bz,color=(0,0,1))
-mlab.plot3d([r0[0],m[0]],[r0[1],m[1]],[r0[2],m[2]],tube_radius=0.001,color=(1,1,0))
-#mlab.plot3d([r0[0],-m[0]],[r0[1],-m[1]],[r0[2],-m[2]],line_width=0.1)
+src = mne.setup_source_space(subject, spacing='oct6',
+                             subjects_dir=subjects_dir, add_dist=False)
+b = mne.viz.plot_alignment(subject=subject,src=src,surfaces=[],
+                           subjects_dir=subjects_dir,meg=["sensors","ref"],
+                           info=raw.info)
+mlab.quiver3d(.2,.1,.3,0.2,-0.3,0.5, figure=b,
+                  color=(1,0,0), mode="arrow", scale_factor=0.3, opacity=1)
+mlab.quiver3d(-.2,-.3,-.5,0.8,-0.1,0.2, figure=b,
+                  color=(1,0,0), mode="arrow", scale_factor=0.3, opacity=1)
+mlab.quiver3d(.5,0,.7,-0.4,-0.6,0.2, figure=b,
+                  color=(1,0,0), mode="arrow", scale_factor=0.3, opacity=1)
